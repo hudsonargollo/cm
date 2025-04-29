b7d7252aadc1eb8d5d6d3911552f59ebb9af701a
Hudson Argollo <105505660+hudsonargollo@users.noreply.github.com>
2025-04-29 03:38:58 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import OnboardingContainer from '@/components/onboarding/OnboardingContainer';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <OnboardingContainer />
     </main>
   );
