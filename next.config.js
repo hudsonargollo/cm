7dca37cd618e9b55de0ed2dc7e332df16540e3c5
Hudson Argollo <105505660+hudsonargollo@users.noreply.github.com>
2025-04-29 09:19:17 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  cssModules: true,
-  swcMinify: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+
+ webpack: (config) => {
+   const rules = config.module.rules
+     .find((rule) => typeof rule.oneOf === 'object')
+    .oneOf.filter((rule) => Array.isArray(rule.use));
+
+   rules.forEach((rule) => {
+     if (rule.test && rule.test.toString().includes('module')) {
+       rule.use.forEach((moduleLoader) => {
+         if (moduleLoader.loader && moduleLoader.loader.includes('css-loader')) {
+           moduleLoader.options.modules = {
+             ...moduleLoader.options.modules,
+             // This ensures class names are more deterministic
+             localIdentName: '[local]__[hash:base64:5]',
+           };
+         }
+       });
+     }
+   });
+  
+   return config;
+ },
 };
 
 module.exports = nextConfig;
